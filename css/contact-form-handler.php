daf773189de169bcee050d6711139c659e67f4d9
jayvo20 <freddievo22@gmail.com>
2020-01-30 17:17:37 -0600

    changes to contact page

---
+++ css/contact-form-handler.php
@@ -0,0 +1,29 @@
+
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_from = 'yourname@yourwebsite.com';
+
+$email_subject = "New Form Message - From Client";
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"User Message: $message.\n";
+
+
+$to = "jaygiavo@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to,email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
+
+
+?>
